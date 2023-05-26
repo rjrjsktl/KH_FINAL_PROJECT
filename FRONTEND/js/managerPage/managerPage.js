616aca2f72a509926233d09d22b714668333658a
MSKY219 <119601001+MSKY219@users.noreply.github.com>
2023-05-26 13:37:47 +0900

    23.05.26 - 오후 1시 38분 - 김민수 - 판리자 페이지 작업중

--- FRONTEND/js/managerPage/managerPage.js
+++ FRONTEND/js/managerPage/managerPage.js
@@ -41,7 +41,7 @@ top_items.on('click', (e) => {
 
 /* 방문자 현황용 그래프 */
 
-var myChart = Highcharts.chart('inner_Main', {
+var myChart = Highcharts.chart('daily_Enter', {
 
     title: {
         text: 'KGV 일일 방문자  수',
@@ -81,7 +81,63 @@ var myChart = Highcharts.chart('inner_Main', {
     responsive: {
         rules: [{
             condition: {
-                maxWidth: 500
+                width: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+});
+/* 일일 영화 관람객 그래프 */
+
+var myChart = Highcharts.chart('daily_Movie_Watch', {
+
+    title: {
+        text: 'KGV 일일 영화 관람객 수',
+        align: 'left'
+    },
+
+    // subtitle: {
+    //     text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
+    //     align: 'left'
+    // },
+
+    yAxis: {
+        title: {
+            text: '일 관람객 (천명)'
+        }
+    },
+
+    xAxis: {
+        type: "datetime",
+    },
+
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle'
+    },
+
+
+    series: [{
+        name: '관람객 수',
+        data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
+            17300, 13053, 11906, 10073],
+        pointStart: Date.UTC(2023, 5, 1),
+        pointInterval: 24 * 3600 * 1000, // one day,
+        color: 'red'
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                width: 500
             },
             chartOptions: {
                 legend: {
