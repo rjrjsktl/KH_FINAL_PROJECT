ecd6a84c27140eb071ff259082c827574cb87a18
cyt <poer131877@gmail.com>
2023-06-01 04:22:16 +0900

    23.06.01 최영탁 회원가입 업데이트

--- BACKEND/MOVIE/src/main/java/com/kh/kgv/login/model/service/SignUpServiceImpl.java
+++ BACKEND/MOVIE/src/main/java/com/kh/kgv/login/model/service/SignUpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.kh.kgv.login.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.kgv.login.model.dao.SignUpDAO;
@@ -7,6 +8,7 @@ import com.kh.kgv.login.model.dao.SignUpDAO;
 @Service
 public class SignUpServiceImpl implements SignUpService {
 	
+	@Autowired
 	private SignUpDAO dao;
 	
 	
