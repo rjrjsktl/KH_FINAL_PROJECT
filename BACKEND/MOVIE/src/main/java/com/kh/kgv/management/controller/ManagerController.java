2cda1109a4590405fa8f6772c46809328e128e34
MSKY219 <119601001+MSKY219@users.noreply.github.com>
2023-06-02 19:58:32 +0900

    23.06.02 - 오후 7시 58분 - 김민수 - 깃 최신화를 위한 커밋

--- BACKEND/MOVIE/src/main/java/com/kh/kgv/management/controller/ManagerController.java
+++ BACKEND/MOVIE/src/main/java/com/kh/kgv/management/controller/ManagerController.java
@@ -19,70 +19,70 @@ public class ManagerController {
 	@GetMapping("/member")
 	public String moveMember() {
 		System.out.println("관리자_회원 리스트 이동");
-		return "manager/member";
+		return "manager/manager_member_list";
 	}
 	
 	// 관리자_1:1 문의 목록 이동
 	@GetMapping("/ask_list")
 	public String moveAskList() {
 		System.out.println("관리자_1:1 문의 목록 이동");
-		return "manager/ask_list";
+		return "manager/manager_ask_list";
 	}
 	
 	// 관리자_영화 목록 이동
 	@GetMapping("/movie_list")
 	public String moveMovieList() {
 		System.out.println("관리자_영화 목록 이동");
-		return "manager/movie_list";
+		return "manager/manager_movie_list";
 	}
 	
 	// 관리자_영화 등록 이동
 	@GetMapping("/movie_add")
 	public String moveMovieAdd() {
 		System.out.println("관리자_영화 등록 이동");
-		return "manager/movie_add";
+		return "manager/manager_movie_add";
 	}
 	
 	// 관리자_상영시간 목록 이동
 	@GetMapping("/play_list")
 	public String movePlayList() {
 		System.out.println("관리자_상영시간 목록 이동");
-		return "manager/play_list";
+		return "manager/manager_movie_play_list";
 	}
 	
 	// 관리자_상영시간 등록 이동
 	@GetMapping("/play_add")
 	public String movePlayAdd() {
 		System.out.println("관리자_상영시간 등록 이동");
-		return "manager/play_add";
+		return "manager/manager_movie_play_add";
 	}
 	
 	// 관리자_이벤트 목록 이동
 	@GetMapping("/event_list")
 	public String moveEventList() {
 		System.out.println("관리자_이벤트 목록 이동");
-		return "manager/event_list";
+		return "manager/manager_event_list";
 	}
 	
 	// 관리자_이벤트 등록 이동
 	@GetMapping("/event_add")
 	public String moveEventAdd() {
 		System.out.println("관리자_이벤트 등록 이동");
-		return "manager/event_add";
+		return "manager/manager_event_add";
 	}
 	
 	// 관리자_공지사항 목록 이동
 	@GetMapping("/notice_list")
 	public String moveNoticeList() {
 		System.out.println("관리자_공지사항 목록 이동");
-		return "manager/notice_list";
+		return "manager/manager_notice_list";
 	}
 	
 	// 관리자_공지사항 등록 이동
 	@GetMapping("/notice_add")
 	public String moveNoticeAdd() {
 		System.out.println("관리자_공지사항 등록 이동");
-		return "manager/notice_add";
+		return "manager/manager_notice_add";
 	}
 	
 }
