7ed89ddec7b900e5976afd2cce6a8bcdd28e285c
last-child <last-child@naver.com>
2023-06-07 21:35:33 +0900

    23.06.07 권지훈 상영관 커스터마이징 작업 중

--- BACKEND/MOVIE/src/main/webapp/resources/js/manager/manager_cinema_add.js
+++ BACKEND/MOVIE/src/main/webapp/resources/js/manager/manager_cinema_add.js
@@ -1,91 +1,178 @@
+// 우클릭 이벤트 해제
+
 window.oncontextmenu = function(){
-	return false;
+  return false;
 }
 
+
+
+// 슬라이더
+
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 5,
-    spaceBetween: 20,
+  slidesPerView: 5,
+  spaceBetween: 20,
 });
 
 
-// 상영관 편집 버튼을 눌렀을 때
 
-let tempNo;
-let tempRow;
-let tempColumn;
-let tempType;
+// 현재 상영관의 번호
+
+let tempNo;   
+
+
+// 현재 상영관의 정보        
+
+let tempRoom = {
+  type: null,
+  maxRow : 1,
+  maxColumn: 1,
+  aisle: [],
+  space: []
+};
+
+
+let tempRowArr = [];
+
+
+
+// [이벤트] 상영관 편집 버튼을 클릭하면
 
 $('.swiper-slide .room_edit').on("click", function(){
-    tempNo = $(this).closest('.swiper-slide').data('room-no');
+  let room = $(this).closest('.swiper-slide');
+  
+  tempNo = $(room).data('room-no');
+  $('#room_area #room_no').html(tempNo);
     
-    $('#room_area #room_no').html(tempNo);
+  tempRoom.maxRow = $(room).data('room-maxrow');
+  $('#temp_row').val(tempRoom.maxRow);
+  
+  tempRoom.maxColumn = $(room).data('room-maxcolumn');
+  $('#temp_column').val(tempRoom.maxColumn);
+  
+  tempRoom.type = $(room).data('room-type');
+  $("#edit_area select").val(tempRoom.type).prop("selected", true);
     
-    tempRow = $(this).closest('.swiper-slide').data('room-maxrow');
-    tempColumn = $(this).closest('.swiper-slide').data('room-maxcolumn');
+  $('#seat_area').empty();
+  createBasicRoom(tempRoom.maxRow, tempRoom.maxColumn);
     
-	$('#seat_area').empty();
-    createBasicRoom(tempRow, tempColumn);
-    
-	$('#temp_row').val(tempRow);
-	$('#temp_column').val(tempColumn);
-	
-	tempType = $(this).closest('.swiper-slide').data('room-type');
-	$("#edit_area select").val(tempType).prop("selected", true);
-	
-    $('#room_area').css('display', 'block');
-    $('#room_area').css('z-index', '3');
+  $('#room_area').css('display', 'block');
+  $('#room_area').css('z-index', '3');
+  
 });
 
 
-// 타입 변경시
+
+// [이벤트] 상영관의 타입을 변경하면
 
 $('#edit_area select').on("input", function(){
-    tempType = $(this).val();
+  tempRoom.type = $(this).val();
 })
 
-// 가로 세로 변경시
 
-$('#edit_area input[type="number"]').on("input", function(){
-  tempRow = $('#temp_row').val();
-  tempColumn = $('#temp_column').val();
+
+// [이벤트] 상영관의 가로줄과 세로줄 수를 변경하면
+
+$('#edit_area .temp_rc').on("input", function(){
+  tempRoom.maxRow = $('#temp_row').val();
+  tempRoom.maxColumn = $('#temp_column').val();
+  
   $('#seat_area').empty();
-  createBasicRoom(tempRow, tempColumn);
+  createBasicRoom(tempRoom.maxRow, tempRoom.maxColumn);
 });
 
 
-// 기본 방 만들기
+
+// [함수] 기본 방 만들기
+
+let alphabet = 'ABCDEFGHIJKLMNOPQLSTUVWXYZ';
 
 function createBasicRoom(maxRow, maxColumn){
-  let s1 = `<a href="#none"><span>`;
-  let s2 = `</span></a>`;
+
+  let s1, s2, row, seat;
+  
+  s1 = `<a href="#none"><span>`;
+  s2 = `</span></a>`;
+  
+  tempRowArr = Array.from({length: maxRow}, () => maxColumn);  
 
   for(let k=1; k<=maxRow; k++){
-    let row = document.createElement('div');
+  
+    row = document.createElement('div');
     document.querySelector('#seat_area').appendChild(row);
+    
     for(let i=1; i<=maxColumn; i++){
-      let seat = document.createElement('a');
+      seat = document.createElement('a');
       seat.href = "#none";
+      $(seat).addClass('seat');
+      $(seat).attr("data-row", k);
+      $(seat).attr("data-column", i);
+      seat.addEventListener("click", function(){
+        clickSeat(this);
+      });
       document.querySelector(`#seat_area > div:nth-child(${k})`).appendChild(seat);
     }
   }
+  
+  console.log(tempRoom);
 }
 
 
-// 좌석 클릭 시
 
-$('#seat_area > div > a').on("click", function(){
-  alert(2);
+// [함수] 좌석을 클릭하면
+
+let right, down;
+
+function clickSeat(s) {
+  $('.seat').removeClass('clicked');
+  $(s).addClass('clicked');
+  $('#seat_option').css('display', 'flex');
+  $('#seat_option select').val("");
+  $('.seat_detail').css('display', 'none');
+   
+  right = tempRoom.maxColumn - $(s).data('column') + 1;
+  down = tempRoom.maxRow - $(s).data('row') + 1;
+
+  $('.seat_detail > input').val("0");
+  $('.seat_right').attr('max', right);
+  $('.seat_down').attr('max', down);
+}
+
+
+
+$('#seat_option select').on("input", function(){
+  $('.seat_detail').css('display', 'none');
+
+  if($(this).val() == '통로 만들기') {
+    createAisle();
+  } 
+  
+  else if($(this).val() == '공간 만들기') {
+    createSpace();
+  }
 });
+
+
+// 통로 만들기
+
+function createAisle(){
+  $('.sd1').css('display', 'flex');
+  
+}
+
+function createSpace() {
+  $('.sd2').css('display', 'flex');
+}
+
 
 
 // 확인 버튼 
 
 $('#room_confirm').on("click", function() {
   let room = $(`.swiper-slide[data-room-no=${tempNo}]`);
-  room.data('room-maxrow', tempRow);
-  room.data('room-maxcolumn', tempColumn);
-  room.data('room-type', tempType);
-  room.find('.room_type').html(tempType);
+  room.data('room-maxrow', tempRoom.maxRow);
+  room.data('room-maxcolumn', tempRoom.maxColumn);
+  room.data('room-type', tempRoom.type);
+  room.find('.room_type').html(tempRoom.type);
 	
   $('#room_area').css('display', 'none');
   $('#room_area').css('z-index', '0');
@@ -95,24 +182,24 @@ $('#room_confirm').on("click", function() {
 // 리셋 버튼
 
 $('#room_reset').on("click", function() {
-    let room = $(`.swiper-slide[data-room-no=${tempNo}]`);
+  let room = $(`.swiper-slide[data-room-no=${tempNo}]`);
     
-    $('#temp_row').val(12);
-    $('#temp_column').val(24);
+  $('#temp_row').val(12);
+  $('#temp_column').val(24);
     
-    tempRow = $('#temp_row').val();
-    tempColumn = $('#temp_column').val();
+  tempRoom.maxRow = $('#temp_row').val();
+  tempRoom.maxColumn = $('#temp_column').val();
     
-    $('#seat_area').empty();
-	createBasicRoom(tempRow, tempColumn);
+  $('#seat_area').empty();
+  createBasicRoom(tempRoom.maxRow, tempRoom.maxColumn);
 	
-	tempType = "일반관";
-	$("#edit_area select").val(tempType).prop("selected", true);
+  tempRoom.type = "일반관";
+  $("#edit_area select").val(tempRoom.type).prop("selected", true);
 	
-	room.data('room-maxrow', tempRow);
-	room.data('room-maxcolumn', tempColumn);
-	room.data('room-type', tempType);
-	room.find('.room_type').html(tempType);
+  room.data('room-maxrow', tempRoom.maxRow);
+  room.data('room-maxcolumn', tempRoom.maxColumn);
+  room.data('room-type', tempRoom.type);
+  room.find('.room_type').html(tempRoom.type);
 
 });
 
