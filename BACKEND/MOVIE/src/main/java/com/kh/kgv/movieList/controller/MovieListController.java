b8169b058a07f5d345734225a1644456125ed661
MSKY219 <119601001+MSKY219@users.noreply.github.com>
2023-06-16 21:50:29 +0900

    23.06.16 - 오후 9시 50분 - 김민수 - 메인 -> 영화 -> 리스트 준비 완료

--- BACKEND/MOVIE/src/main/java/com/kh/kgv/movieList/controller/MovieListController.java
+++ BACKEND/MOVIE/src/main/java/com/kh/kgv/movieList/controller/MovieListController.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.kh.kgv.items.model.vo.Movie;
 import com.kh.kgv.movieList.model.service.MovieService;
 
 @Controller
@@ -19,16 +21,33 @@ public class MovieListController {
 	@Autowired
 	private MovieService service;
 	
+//	메인페이지 -> 영화 -> 무비차트 이동 시 영화 목록 조회
 	@RequestMapping("/detail_List")
 	public String moveList(Model model) {
 		
-		Movie getMovieList  = service.movieList();
-		
+		Map<String, Object>getMovieList = null;
+		System.out.println("DB에서 가지고온 getMovieList : =============================================" + getMovieList);
+		getMovieList = service.movieList();
 		
 		model.addAttribute("getMovieList", getMovieList);
 		
 		
 		return "movieList/detail_List";
 	
+	}
+	
+	// 영화 세부내용
+	@RequestMapping("/detail_List/introduce/{movieNo}")
+	public String getMovieDetail(
+			Model model
+			, @PathVariable("movieNo") int movieNo
+			) {
+		
+		System.out.println("영화 상세페이지로 이동함");
+		System.out.println("movieNo ============================" + movieNo);
+		
+		return null;
+		
+		
 	}
 }
