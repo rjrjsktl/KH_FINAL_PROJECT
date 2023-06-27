97c4bf59aade97b568136666c61854d0528fcd5f
MSKY219 <119601001+MSKY219@users.noreply.github.com>
2023-06-27 21:44:48 +0900

    23.06.27 - 오후 9시 44분 - 김민수 - 페이지네이션 5개로 제한 및 누적 영화 상영 횟수 추가

--- BACKEND/MOVIE/src/main/java/com/kh/kgv/movieList/model/service/MovieService.java
+++ BACKEND/MOVIE/src/main/java/com/kh/kgv/movieList/model/service/MovieService.java
@@ -9,7 +9,7 @@ public interface MovieService {
 //	메인페이지 -> 영화 -> 상영중인영화 이동 시 영화 목록 조회
 	Map<String, Object> movieList();
 
-	// 메인 상영중인 영화 목록 가지고 오기 - 7개
+	// 메인 상영중인 영화 목록 가지고 오기 - 6개
 	Map<String, Object> mainMovieList();
 
 	// 관리자 페이지 상영 조회
