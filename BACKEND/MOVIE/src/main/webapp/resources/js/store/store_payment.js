53f59d7dd91d16d559c142fe07606abfcc90fb83
cyt <poer131877@gmail.com>
2023-06-29 06:01:47 +0900

    23.06.29 최영탁 커밋

--- BACKEND/MOVIE/src/main/webapp/resources/js/store/store_payment.js
+++ BACKEND/MOVIE/src/main/webapp/resources/js/store/store_payment.js
@@ -11,19 +11,18 @@ $(document).ready(function () {
 });
 
 
-
+const storeNo = $('#storeNo');
 const userEmail = $('#userEmail').val();
 console.log(userEmail);
 const userName = $('#userName').val();
 console.log(userName);
-// const totalPrice = $('#totalPrice').val();
-// console.log(totalPrice);
 var totalPrice = parseInt($('#totalPrice').text().replace(/,/g, ''));
 console.log('결제 금액:', totalPrice);
 var storeName = $("#storeName").text();
-
-// storeName 변수를 활용하여 필요한 로직을 구현합니다.
 console.log('스토어 이름:', storeName);
+
+
+
 
 function requestPay() {
     IMP.request_pay(
@@ -37,18 +36,21 @@ function requestPay() {
             buyer_name: userName
 
         },
-        function (res) {
+        function (rsp) {
 
 
-
+            console.log(rsp);
 
             // 결제검증
             $.ajax({
+
                 type: "POST",
-                url: "/verifyIamport/" + res.imp_uid
+                url: "/movie/store/storeMain/store_detail/" + storeNo.val() + "/getStorePayment/verifyIamport/" + rsp.imp_uid
             }).done(function (data) {
 
-                if (res.paid_amount == data.response.amount) {
+                console.log(data);
+
+                if (rsp.paid_amount == data.response.amount) {
                     alert("결제 및 결제검증완료");
 
                     //결제 성공 시 비즈니스 로직
@@ -57,11 +59,7 @@ function requestPay() {
                     alert("결제 실패");
                 }
             });
-
-
-
-        }
-    );
+        });
 }
 
 
