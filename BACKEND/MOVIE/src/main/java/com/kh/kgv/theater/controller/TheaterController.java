aface95dade2fdce56f06191753339a8f90e9052
MSKY219 <119601001+MSKY219@users.noreply.github.com>
2023-06-28 13:45:40 +0900

    23.06.28 - 오후 1시 44분 - 김민수 - 로고 관련 변경 추가

--- BACKEND/MOVIE/src/main/java/com/kh/kgv/theater/controller/TheaterController.java
+++ BACKEND/MOVIE/src/main/java/com/kh/kgv/theater/controller/TheaterController.java
@@ -8,12 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.kh.kgv.management.model.service.ManagerService;
 
 
 @Controller
 @RequestMapping("/theater")
+@SessionAttributes({ "loginUser" })
 public class TheaterController {
 
 	@Autowired
